08eb911f08eac1c00fef729acd3d14c7d481c512
David Grier <david.grier@nyu.edu>
2018-11-17 15:32:25 -0500

    docstrings

--- QPolargraph/Polargraph.py
+++ QPolargraph/Polargraph.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from common.SerialDevice import SerialDevice
+from qacam.common.SerialDevice import SerialDevice
 import numpy as np
 from time import sleep
 
@@ -10,7 +10,31 @@ logger.setLevel(logging.INFO)
 
 
 class Motors(SerialDevice):
-    """Control the pair of stepper motors driving a polargraph"""
+    '''
+    Abstraction of stepper moters controlled by Arduino
+
+    ...
+
+    Attributes
+    ----------
+    indexes : tuple of integers
+        (n1, n2) Step indexes of two stepper motors.
+        Setting this property causes the motors to move to (n1, n2).
+    motor_speed : float
+        maximum stepper motor speed in steps/second.
+
+    Methods
+    -------
+    goto(n1, n2)
+        Set target indexes for stepper motors.  This causes the
+        motors to move from their present indexes to the new values.
+    home()
+        Equivalent to goto(0, 0)
+    release()
+        Stop motors and turn off current to coils.
+    running() : bool
+        Returns True if motors are running.
+    '''
 
     def __init__(self):
         super(Motors, self).__init__(eol='\n',
@@ -26,19 +50,27 @@ class Motors(SerialDevice):
         return acam
 
     def goto(self, n1, n2):
-        """Move to index (n1, n2)"""
+        '''Move to index (n1, n2)
+
+        Parameters
+        ----------
+        n1 : int
+            Index of motor 1
+        n2 : int
+            Index of motor 2
+        '''
         self.command('G:%d:%d' % (n1, n2))
 
     def home(self):
-        """Move to home position"""
+        '''Move to home position'''
         self.goto(0, 0)
 
     def release(self):
-        """Stop and release motors"""
+        '''Stop and release motors'''
         self.command('S')
 
     def running(self):
-        """Returns True if motors are running"""
+        '''Returns True if motors are running'''
         try:
             res = self.command('R')
             header, running = res.split(':')
@@ -49,7 +81,7 @@ class Motors(SerialDevice):
 
     @property
     def indexes(self):
-        """Returns current step numbers for motors"""
+        '''Current step numbers for motors'''
         try:
             res = self.command('P')
             header, n1, n2 = res.split(':')
@@ -67,7 +99,7 @@ class Motors(SerialDevice):
 
     @property
     def motor_speed(self):
-        """Maximum motor speed in steps/s"""
+        '''Maximum motor speed in steps/sec'''
         try:
             res = self.command('V')
             header, speed = res.split(':')
@@ -83,14 +115,56 @@ class Motors(SerialDevice):
 
 
 class Polargraph(Motors):
-    """Control a polargraph
+    '''
+    Abstraction of a polargraph
 
     The polargraph consists of two stepper motors with GT2 gears
     that translate a GT2 timing belt.  The motors are controlled
     by an Arduino microcontroller that is connected to the host
     computer by USB.  This class communicates with the Arduino to
     obtain programmed motion from the motors.
-    """
+
+    ...
+
+    Attributes
+    ----------
+    unit : float
+        Separation between teeth on timing belt [mm]
+        Default: 2.
+    circumference : float
+        Number of gear teeth per revolution.
+        Default: 25.
+    steps : float
+        Number of motor steps per revolution.
+        Default: 200.
+    ell : float
+        Separation between motors [m]
+    y0 : float
+        Vertical displacement of home position [m]
+    y1 : float
+        Vertical position of scan start [m]
+    width : float
+        Width of scan area [m]
+    height : float
+        Height of scan area [m]
+    dy : float
+        Vertical displacement between scan lines [m]
+    ds : float
+        Distance traveled per motor step [m]
+    s0 : float
+        Length of belt from motor to payload at home position [m]
+    position : (x, y)
+       Report current coordinates of payload measured in 
+       meters from home position.
+    speed : float
+       Translation speed [mm/s]
+
+    Methods
+    -------
+    goto(x, y)
+       Move payload to coordinates (x, y), measured in meters
+       from home position.
+    '''
 
     def __init__(self,
                  unit=2.,  # size of one timing belt tooth [mm]
@@ -122,16 +196,16 @@ class Polargraph(Motors):
 
     @property
     def ds(self):
-        """distance traveled per step [m]"""
+        '''Distance traveled per step [m]'''
         return 1e-3 * self.unit * self.circumference / self.steps
 
     @property
     def s0(self):
-        """distance from motor to payload at home position [m]"""
+        '''Distance from motor to payload at home position [m]'''
         return np.sqrt((self.ell / 2.)**2 + (self.y0)**2)
-        
+
     def goto(self, x, y):
-        """Move payload to position (x,y)"""
+        '''Move payload to position (x,y)'''
         s1 = np.sqrt((self.ell / 2. - x)**2 + y**2)
         s2 = np.sqrt((self.ell / 2. + x)**2 + y**2)
         n1 = np.rint((s1 - self.s0) / self.ds).astype(int)
@@ -140,7 +214,7 @@ class Polargraph(Motors):
 
     @property
     def position(self):
-        """Current coordinates in meters"""
+        '''Current coordinates in meters'''
         n1, n2 = self.indexes
         s1 = self.s0 + n1*self.ds
         s2 = self.s0 - n2*self.ds
@@ -155,7 +229,7 @@ class Polargraph(Motors):
 
     @property
     def speed(self):
-        """Translation speed in mm/s"""
+        '''Translation speed in mm/s'''
         return self.motor_speed * self.circumference * self.unit / self.steps
 
     @speed.setter
