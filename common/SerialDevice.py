0abf65b97218851e42ef99ab09a0abdcdae84f64
David Grier <david.grier@nyu.edu>
2018-11-17 15:10:02 -0500

    docstrings

--- common/SerialDevice.py
+++ common/SerialDevice.py
@@ -12,6 +12,55 @@ logger.setLevel(logging.WARNING)
 
 
 class SerialDevice(object):
+    '''
+    Abstraction of an instrument connected to a serial port
+
+    ...
+
+    Attributes
+    ----------
+    eol : str, optional
+        End-of-line character.
+        Default: '\r' (carriage return)
+    manufacturer : str, optional
+        Identifier for the serial interface manufacturer.
+        Default: 'Prolific'
+    baudrate : int, optional
+        Baud rate for serial communication.
+        Default: 9600
+    parity : int, optional
+        One of the constants defined in the serial package
+    stopbits : int, optional
+        One of the constants defined in the serial package
+    timeout : float
+        Read timeout period [s].
+        Default: 0.1
+
+    Methods
+    -------
+    find() : bool
+        Find the serial device that satisfies identify().
+        Returns True if the device is found and correctly opened.
+    identify() : bool
+        Returns True if the device on the opened port correctly
+        identifies itself.  Subclasses must override this method.
+    close()
+        Close serial device
+    write(str)
+        Write str to serial device and flush serial port.
+    readln()
+        Read data from the serial port until EOL character.
+    available() : bool
+        True if characters are available for reading.
+    command(cmd) : string
+        Send cmd to device and return response as string.
+    busy() : bool
+        Returns True if the device is executing a command.
+        This method should be implemented by a subclass.
+    error() : bool
+        Returns True if the device has encountered an error.
+        This method should be implemented by a subclass.
+    '''
 
     def __init__(self,
                  eol='\r',
@@ -32,6 +81,14 @@ class SerialDevice(object):
             raise ValueError('Could not find serial device')
 
     def find(self):
+        '''
+        Attempt to identify and open the serial port
+
+        Returns
+        -------
+        find : bool
+            True if port identified and successfully opened.
+        '''
         ports = list(comports())
         if len(ports) <= 0:
             logger.warning('No serial ports identified')
@@ -72,40 +129,79 @@ class SerialDevice(object):
         return False
 
     def identify(self):
+        '''
+        Identify this device
+
+        Subclasses must override this method
+
+        Returns
+        -------
+        identify : bool
+            True if attached device correctly identifies itself.
+        '''
         return False
 
     def close(self):
+        '''Close serial device'''
         self.ser.close()
 
     def write(self, str):
+        '''
+        Write string to serial device
+
+        Parameters
+        ----------
+        str : string
+            String to be transferred
+        '''
         logger.debug('write: {}'.format(str))
         self.ser.write((str + self.eol).encode())
         self.ser.flush()
 
     def readln(self):
+        '''
+        Read one line from serial port
+
+        Returns
+        -------
+        str : string
+            Available data from serial port
+        '''
         str = self.ser.read_until(terminator=self.eol)
         str = str.decode().strip()
         logger.debug('read: {}'.format(str))
         return str
 
     def available(self):
+        '''
+        Return True if serial characters are available
+        '''
         return self.ser.in_waiting
 
     def command(self, cmd):
-        """Send command to device and return response"""
+        '''
+        Send command to device and return response
+
+        Returns
+        -------
+        response : string
+            Response from device
+        '''
         self.write(cmd)
         return self.readln()
 
     def busy(self):
-        """Return True if device is executing a command
+        '''
+        Return True if device is executing a command
 
         A subclass can override this method.
-        """
+        '''
         return False
 
     def error(self):
-        """Return True if device has an error condition
+        '''
+        Return True if device has an error condition
 
         A subclass can override this method.
-        """
+        '''
         return False
